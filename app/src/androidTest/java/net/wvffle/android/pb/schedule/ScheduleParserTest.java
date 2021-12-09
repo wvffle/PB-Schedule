f0f2fae38b2b7a4fb542823d09276e6fc4323f38
Kasper Seweryn <github@wvffle.net>
2021-12-09 23:42:27 +0100

    feat(api): add updatedAt to rooms

--- app/src/androidTest/java/net/wvffle/android/pb/schedule/ScheduleParserTest.java
+++ app/src/androidTest/java/net/wvffle/android/pb/schedule/ScheduleParserTest.java
@@ -20,7 +20,13 @@ public class ScheduleParserTest {
     public void canParseXMLData () {
         List<ScheduleEntry> entries = new ScheduleParser(new ScheduleUpdater().getScheduleData()).parse();
         assertNotEquals(0, entries.size());
-        assertEquals("WA-23C-1", ((Room) entries.get(0)).getName());
-        assertEquals(1, ((Room) entries.get(0)).getId());
+        for (ScheduleEntry entry : entries) {
+            if (entry instanceof Room) {
+                Room room = (Room) entry;
+                assertNotEquals("", room.getName());
+                assertTrue(room.getId() > -1);
+                assertTrue(room.getUpdatedAt() > -1);
+            }
+        }
     }
 }
