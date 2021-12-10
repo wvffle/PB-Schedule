2b1a834a2ec80bc78c85ef67331cef5c2473c4bd
Sylwia0506 <sysazeszkoly@gmail.com>
2021-12-10 21:29:47 +0100

    feat(api): add class title

---
+++ app/src/main/java/net/wvffle/android/pb/schedule/api/db/Title.java
@@ -0,0 +1,27 @@
+package net.wvffle.android.pb.schedule.api.db;
+
+
+public class Title extends ScheduleEntry {
+    public static final String TAG_NAME = "tabela_tytuly";
+    private final int id;
+    private final String name;
+    private final int updatedAt;
+
+    public Title(int id, String name, int updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUpdatedAt() {
+        return updatedAt;
+    }
+}
