d283f9cd2906509ad5777353c128f8613d076327
Sylwia0506 <sysazeszkoly@gmail.com>
2021-12-12 00:43:02 +0100

    fix(api): fix class room

--- app/src/main/java/net/wvffle/android/pb/schedule/api/db/Room.java
+++ app/src/main/java/net/wvffle/android/pb/schedule/api/db/Room.java
@@ -1,65 +1,30 @@
 package net.wvffle.android.pb.schedule.api.db;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.IOException;
+import java.util.Date;
 
-public class Room extends ScheduleEntry {
-    public static final String TAG_NAME = "tabela_sale";
-    private final int id;
+public class Room  {
+    private final String hash ;
     private final String name;
-    private final int updatedAt;
+    private final Date updatedAt;
 
-    public Room(int id, String name, int updatedAt) {
-        this.id = id;
+    public Room(String hash, String name, int updatedAt) {
+        this.hash = hash;
         this.name = name;
-        this.updatedAt = updatedAt;
+        this.updatedAt = new Date(updatedAt);
     }
 
-    public static Room fromParser (XmlPullParser parser) throws IOException, XmlPullParserException {
-        int id = -1;
-        String name = "";
-        int updatedAt = -1;
-
-        parser.require(XmlPullParser.START_TAG, null, TAG_NAME);
-        updatedAt = Integer.parseInt(parser.getAttributeValue(null, "data-aktualizacji")) * 1000;
-
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-
-            switch (parser.getName()) {
-                case "ID":
-                    parser.require(XmlPullParser.START_TAG, null, "ID");
-                    id = Integer.parseInt(readText(parser));
-                    parser.require(XmlPullParser.END_TAG, null, "ID");
-                    break;
-
-                case "NAZWA":
-                    parser.require(XmlPullParser.START_TAG, null, "NAZWA");
-                    name = readText(parser);
-                    parser.require(XmlPullParser.END_TAG, null, "NAZWA");
-                    break;
-
-                default: skip(parser);
-            }
-        }
-
-
-        return new Room(id, name, updatedAt);
+    public String getHash() {
+        return hash;
     }
 
-    public int getId() {
-        return id;
-    }
 
     public String getName() {
         return name;
     }
 
-    public int getUpdatedAt() {
+    public Date getUpdatedAt() {
         return updatedAt;
     }
+
 }
