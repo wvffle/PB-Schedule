fb8c7bb8e61c8c6caa4e0f98c4dd26a3aa99458f
Sylwia0506 <sysazeszkoly@gmail.com>
2021-12-11 21:53:30 +0100

    feat(api): add class Degree

---
+++ app/src/main/java/net/wvffle/android/pb/schedule/api/db/Degree.java
@@ -0,0 +1,22 @@
+package net.wvffle.android.pb.schedule.api.db;
+
+public class Degree {
+    private String hash;
+    private String name;
+
+    public String getHash() {
+        return null;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
