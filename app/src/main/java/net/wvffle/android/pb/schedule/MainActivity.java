aab2a9c2adc48e486c6cb594cdd6c642bae28c3f
Kasper Seweryn <github@wvffle.net>
2021-12-14 00:19:59 +0100

    fix: replace rooms with the same hash on conflict

--- app/src/main/java/net/wvffle/android/pb/schedule/MainActivity.java
+++ app/src/main/java/net/wvffle/android/pb/schedule/MainActivity.java
@@ -9,7 +9,10 @@ import android.util.Log;
 
 import net.wvffle.android.pb.schedule.api.ApiWorker;
 import net.wvffle.android.pb.schedule.api.BackendApi;
+import net.wvffle.android.pb.schedule.api.db.ObjectBox;
 import net.wvffle.android.pb.schedule.api.syncedcollectionentry.entries.Room;
+
+import io.objectbox.Box;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         WorkManager.getInstance(this).enqueue(ApiWorker.create(() -> {
             Room room = BackendApi.getRoom("00d1c9c8a3668121c9fc4d89b848b17e73a61073");
+            Log.d("Backend", String.valueOf(room.id));
             Log.d("Backend", room.getHash());
             Log.d("Backend", room.getName());
+
+            Box<Room> box = ObjectBox.get().boxFor(Room.class);
+            box.put(room);
+
+            Log.d("ObjectBox", String.valueOf(room.id));
+            Log.d("ObjectBox", room.getHash());
+            Log.d("ObjectBox", room.getName());
+
             return ListenableWorker.Result.success();
         }));
     }
