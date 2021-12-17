758125985e525decbcc07a55570349f4d9ccbf3d
Kasper Seweryn <github@wvffle.net>
2021-12-17 11:25:21 +0100

    test: add update test

---
+++ app/src/test/java/net/wvffle/android/pb/schedule/UpdateModelTest.java
@@ -0,0 +1,50 @@
+package net.wvffle.android.pb.schedule;
+
+import net.wvffle.android.pb.schedule.api.BackendApi;
+import net.wvffle.android.pb.schedule.api.update.UpdateData;
+import net.wvffle.android.pb.schedule.api.update.UpdateDiff;
+import net.wvffle.android.pb.schedule.api.update.UpdateEntry;
+import net.wvffle.android.pb.schedule.models.Update;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class UpdateModelTest {
+
+    @Test
+    public void canFetchUpdates () {
+        List<UpdateEntry> updates = BackendApi.getUpdates();
+        assertTrue(updates.size() > 0);
+    }
+
+    @Test
+    public void canFetchSingleUpdate () {
+        List<UpdateEntry> updates = BackendApi.getUpdates();
+        Update update = BackendApi.getUpdate(updates.get(0).getHash());
+
+        assertNotNull(update);
+        assertEquals(updates.get(0).getHash(), update.getHash());
+        assertEquals(updates.get(0).getId(), update.id);
+
+        UpdateData data = update.getData();
+        assertNotNull(data);
+        assertTrue(data.getRooms().size() > 0);
+
+        UpdateDiff diff = update.getDiff();
+        assertNotNull(diff);
+
+        boolean hasAdded = diff.getAdded().size() > 0;
+        boolean hasRemoved =  diff.getRemoved().size() > 0;
+        assertTrue(hasAdded || hasRemoved);
+
+        if (hasAdded) {
+            assertTrue(diff.getAddedModels().size() > 0);
+        }
+
+        if (hasRemoved) {
+            assertTrue(diff.getRemovedModels().size() > 0);
+        }
+    }
+}
