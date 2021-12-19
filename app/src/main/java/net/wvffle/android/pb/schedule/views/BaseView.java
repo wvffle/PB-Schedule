190ae606ce27bca97848d7a21b3d6f1524056a8c
Kasper Seweryn <github@wvffle.net>
2021-12-19 17:34:35 +0100

    feat: add default animations
    Closes: #21

--- app/src/main/java/net/wvffle/android/pb/schedule/views/BaseView.java
+++ app/src/main/java/net/wvffle/android/pb/schedule/views/BaseView.java
@@ -11,7 +11,11 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavAction;
+import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 
 import net.wvffle.android.pb.schedule.MainActivity;
@@ -43,7 +47,32 @@ public abstract class BaseView<B extends ViewDataBinding> extends Fragment {
         return Navigation.findNavController(MainActivity.getInstance(), R.id.fragmentContainerView);
     }
 
+    private static int or(int resId1, int resId2) {
+        return resId1 == -1 ? resId2 : resId1;
+    }
+
     protected void navigate(int resId) {
-        getNavController().navigate(resId);
+        NavController controller = getNavController();
+
+        NavBackStackEntry stackEntry = controller.getCurrentBackStackEntry();
+        NavDestination destination = stackEntry == null
+                ? controller.getCurrentDestination()
+                : stackEntry.getDestination();
+
+        NavAction action = destination.getAction(resId);
+        NavOptions oldOptions = action == null ? null : action.getNavOptions();
+
+        NavOptions options = oldOptions == null
+                ? null // TODO: Default nav options
+                : new NavOptions.Builder()
+                .setLaunchSingleTop(oldOptions.shouldLaunchSingleTop())
+                .setPopUpTo(resId, oldOptions.isPopUpToInclusive())
+                .setEnterAnim(or(oldOptions.getEnterAnim(), R.anim.nav_default_enter_anim))
+                .setExitAnim(or(oldOptions.getExitAnim(), R.anim.nav_default_exit_anim))
+                .setPopEnterAnim(or(oldOptions.getPopEnterAnim(), R.anim.nav_default_pop_enter_anim))
+                .setPopExitAnim(or(oldOptions.getPopExitAnim(), R.anim.nav_default_pop_exit_anim))
+                .build();
+
+        controller.navigate(resId, null, options);
     }
 }
