4e4be6eaeb2c5063f814c3dfa2500ddf42adcd59
Kasper Seweryn <github@wvffle.net>
2021-12-20 02:34:02 +0100

    feat: add first step view

--- app/src/main/java/net/wvffle/android/pb/schedule/viewmodels/SetupViewModel.java
+++ app/src/main/java/net/wvffle/android/pb/schedule/viewmodels/SetupViewModel.java
@@ -6,9 +6,25 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import net.wvffle.android.pb.schedule.MainActivity;
+import net.wvffle.android.pb.schedule.ObjectBox;
+import net.wvffle.android.pb.schedule.models.Degree;
+import net.wvffle.android.pb.schedule.models.Update;
+import net.wvffle.android.pb.schedule.models.Update_;
+
+import java.util.List;
+
+import io.objectbox.query.QueryBuilder;
 
 public class SetupViewModel extends ViewModel {
     private final MutableLiveData<String> buttonName = new MutableLiveData<>("");
+    private final MutableLiveData<Update> update = new MutableLiveData<>(
+            ObjectBox.getUpdateBox()
+                    .query()
+                    .order(Update_.id, QueryBuilder.DESCENDING)
+                    .build()
+                    .findFirst()
+    );
+    private final MutableLiveData<List<Degree>> degrees = new MutableLiveData<>();
 
     public LiveData<String> getButtonName() {
         return buttonName;
@@ -20,5 +36,17 @@ public class SetupViewModel extends ViewModel {
 
     public void setButtonName(@StringRes int resId) {
         setButtonName(MainActivity.getInstance().getString(resId));
+    }
+
+    public MutableLiveData<Update> getUpdate() {
+        return update;
+    }
+
+    public MutableLiveData<List<Degree>> getDegrees() {
+        return degrees;
+    }
+
+    public void setDegrees(List<Degree> degreeList) {
+        degrees.setValue(degreeList);
     }
 }
