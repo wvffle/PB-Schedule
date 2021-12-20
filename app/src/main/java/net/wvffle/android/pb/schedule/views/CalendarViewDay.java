915d48fc59f1b3fb9faf673562730bcc8c92f9ff
Sylwia0506 <sysazeszkoly@gmail.com>
2021-12-20 17:01:43 +0100

    feat: add calendar day view

---
+++ app/src/main/java/net/wvffle/android/pb/schedule/views/CalendarViewDay.java
@@ -0,0 +1,92 @@
+package net.wvffle.android.pb.schedule.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import net.wvffle.android.pb.schedule.R;
+
+import java.util.Calendar;
+
+
+class CalendarDayView extends FrameLayout {
+
+    private int mDayHeight = 0;
+
+    private int mEventMarginLeft = 0;
+
+    private int mHourWidth = 120;
+
+    private int mTimeHeight = 120;
+
+    private int mSeparateHourHeight = 0;
+
+    private int mStartHour = 0;
+
+    private int mEndHour = 24;
+
+    private LinearLayout mLayoutDayView;
+
+
+    public CalendarDayView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CalendarDayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CalendarDayView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        LayoutInflater.from(getContext()).inflate(R.layout.view_day_calendar, this, true);
+
+        mLayoutDayView = (LinearLayout) findViewById(R.id.dayview_container);
+
+
+        refresh();
+    }
+
+    public void refresh() {
+        drawDayViews();
+
+    }
+
+    private void drawDayViews() {
+        mLayoutDayView.removeAllViews();
+        DayView dayView = null;
+        for (int i = mStartHour; i <= mEndHour; i++) {
+            mLayoutDayView.addView(dayView);
+        }
+        mHourWidth = (int) dayView.getHourTextWidth();
+        mTimeHeight = (int) dayView.getHourTextHeight();
+        mSeparateHourHeight = (int) dayView.getSeparateHeight();
+    }
+
+
+    private int getPositionOfTime(Calendar calendar) {
+        int hour = calendar.get(Calendar.HOUR_OF_DAY) - mStartHour;
+        int minute = calendar.get(Calendar.MINUTE);
+        return hour * mDayHeight + minute * mDayHeight / 60;
+    }
+
+
+    public void setLimitTime(int startHour, int endHour) {
+        if (startHour >= endHour) {
+            throw new IllegalArgumentException("start hour must before end hour");
+        }
+        mStartHour = startHour;
+        mEndHour = endHour;
+        refresh();
+    }
+
+
+}
+
